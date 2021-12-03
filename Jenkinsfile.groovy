25d9a2837bc6b58f150b5627ca2ef96fad734cea
Makis Karadimas <makis.karadimas@intrasoft-intl.com>
2021-12-03 13:15:58 +0200

    Fix Jenkinsfile

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -5,7 +5,6 @@ pipeline {
     ansiColor('xterm') // Enable colors in terminal
     timestamps() // Append timestamps to each line
     timeout(time: 20, unit: 'MINUTES') // Set a timeout on the total execution time of the job
-    skipDefaultCheckout(true)
   }
   stages {  // Define the individual processes, or stages, of your CI pipeline
     stage('Build') { // Checkout (git clone ...) the projects repository
