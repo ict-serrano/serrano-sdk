1b2751f1669d5f28f13f14fb8c1374d1ebf15740
Makis Karadimas <makis.karadimas@intrasoft-intl.com>
2022-10-25 00:17:00 +0300

    Update Jenkinsfile

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,4 +1,7 @@
 pipeline {
+  environment {
+    PROJECT_NAME = 'serrano-sdk'
+  }
   options {
     buildDiscarder(logRotator(numToKeepStr: '10')) // Retain history on the last 10 builds
     ansiColor('xterm') // Enable colors in terminal
@@ -19,6 +22,42 @@ pipeline {
           sh '/usr/local/bin/python -m pip install --upgrade pip'
           sh 'pip install -r requirements.txt'
           sh 'pip install --no-input cyclonedx-bom'
+        }
+      }
+    }
+    stage('Unit tests') {
+      steps {
+        container('python') {
+          sh 'python -m unittest composeexample.utils'
+        }
+      }
+    }
+    stage('Sonarqube') {
+      environment {
+        scannerHome = tool 'SonarQubeScanner'
+      }
+      steps {
+        container('java') {
+          withSonarQubeEnv('sonarqube') {
+            sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=${PROJECT_NAME}"
+          }
+          timeout(time: 10, unit: 'MINUTES') {
+            waitForQualityGate abortPipeline: true
+          }
+        }
+      }
+    }
+    stage('Generate BOM') {
+      steps {
+        container('python') {
+          sh 'cyclonedx-bom -e -F -o ./bom.xml'
+        }
+      }
+    }
+    stage('Dependency Track') {
+      steps {
+        container('python') {
+          dependencyTrackPublisher artifact: 'bom.xml', projectId: '282b5d5e-547b-449b-a04b-ff8b049f07ee', synchronous: true
         }
       }
     }
