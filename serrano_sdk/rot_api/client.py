91b23cdf941126fea2ad78f3b38661997ee67d85
Makis Karadimas <makis.karadimas@netcompany-intrasoft.com>
2023-01-22 22:40:28 +0200

    Add ROT client

--- serrano_sdk/rot_api/client.py
+++ serrano_sdk/rot_api/client.py
@@ -1,3 +1,5 @@
+import json
+import os.path
 import requests
 
 class ROT:
@@ -8,22 +10,92 @@ class ROT:
     :type parameter: int
     """
 
-    def __init__(self, multiplier):
-        self.multiplier = multiplier
+    def __init__(self, config_path):
         self.session = requests.Session()
 
-    def employ(self):
-        """
-        Employ ROT using a given parameter.
+        if config_path:
+            self.__load_configuration(config_path)
+        else:
+            self.__load_configuration("%s/.serrano/sdk_config.json" % (os.path.expanduser("~")))
 
-        :param parameter: The parameter to use.
-        :type parameter: int
+    def __load_configuration(self, config_file):
 
-        :return: The result of calling telemetry.
-        :rtype: object
-        """
-        response =self.session.get("http://api.open-notify.org/astros.json")
+        try:
+            with open(config_file) as f:
+                params = json.load(f)
+
+                self.__client_uuid = params["rot"]["api_client"]["client_uuid"]
+                self.__databroker = params["rot"]["databroker_interface"]
+
+                self.__rest_url = "https://%s:%s" % (params["rot"]["api_client"]["server_address"],
+                                                    params["rot"]["api_client"]["server_port"])
+
+                self.__http_auth = (params["rot"]["api_client"]["username"], params["rot"]["api_client"]["password"])
+
+        except FileNotFoundError:
+            raise clientEvents.ConfigurationError("Invalid configuration - FileNotFoundError")
+        except json.JSONDecodeError as s:
+            raise clientEvents.ConfigurationError("Invalid configuration - JSONDecodeError: %s" % s.msg)
+        except KeyError as s:
+            raise clientEvents.ConfigurationError("Invalid configuration - Missing configuration parameter %s" % s)
+
+    def is_ready(self):
+        response =self.session.get(self.__rest_url, verify=False)
         return response
+    def get_engines(self):
+        data = {}
+        res = requests.get("%s/api/v1/rot/engines" % self.__rest_url, auth=self.__http_auth)
+        if res.status_code == 200:
+            data = json.loads(res.text)["engines"]
+        return data
+
+    def get_engine(self, engine_uuid):
+        data = {}
+        res = requests.get("%s/api/v1/rot/engine/%s" % (self.__rest_url, engine_uuid), auth=self.__http_auth)
+        if res.status_code == 200:
+            data = json.loads(res.text)
+        return data
+
+    def get_logs(self, execution_uuid):
+        data = {}
+        res = requests.get("%s/api/v1/rot/logs/%s" % (self.__rest_url, execution_uuid), auth=self.__http_auth)
+        if res.status_code == 200:
+            data = json.loads(res.text)["log_details"]
+        return data
+
+    def get_statistics(self, **kwargs):
+        start = kwargs.get('start', None)
+        end = kwargs.get('end', None)
+        return {}
+
+    def delete_execution(self, execution_uuid):
+        res = requests.delete("%s/api/v1/rot/execution/%s" % (self.__rest_url, execution_uuid), auth=self.__http_auth)
+        print(res.text)
+
+    def post_execution(self, execution_plugin, parameters):
+        data = None
+        if type(parameters) is not dict:
+            parameters = json.loads(parameters)
+        res = requests.post("%s/api/v1/rot/execution" % self.__rest_url,
+                            auth=self.__http_auth,
+                            json={"execution_plugin": execution_plugin, "parameters": parameters})
+        if res.status_code == 200 or res.status_code == 201:
+            data = json.loads(res.text)
+        return data
+
+    def get_execution(self, execution_uuid):
+        data = {}
+        res = requests.get("%s/api/v1/rot/execution/%s" % (self.__rest_url, execution_uuid), auth=self.__http_auth)
+        if res.status_code == 200:
+            data = json.loads(res.text)
+        return data
+
+    def get_executions(self):
+        data = {}
+        res = requests.get("%s/api/v1/rot/executions" % self.__rest_url, auth=self.__http_auth)
+        if res.status_code == 200:
+            data = json.loads(res.text)["executions"]
+        return data
 
     def close(self):
         self.session.close()
--- tests/rot_api/testclient.py
+++ tests/rot_api/testclient.py
@@ -1,17 +1,17 @@
 import unittest
-
+import os.path
 from serrano_sdk.rot_api.client import ROT
 
 
 class ROTTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.rot_api = ROT(2)
+        self.rot_api = ROT(os.path.join(os.path.dirname(__file__), "sdk_config.json"))
 
     def test_employ(self):
         """Test employ"""
 
-        result = self.rot_api.employ()
+        result = self.rot_api.is_ready()
         self.assertEqual(result.status_code, 200)
         self.rot_api.close()
 
